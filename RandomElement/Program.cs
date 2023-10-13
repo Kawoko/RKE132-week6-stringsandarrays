e2e2133f03a2979ec77b4debb490445c2f1a7684
Kawoko <rasmus.varavas@tktk.ee>
2023-10-13 17:16:15 +0300

    Added stuff

---
+++ RandomElement/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] snacks = { "chips", "cookies", "candy", "ice cream", "Asian takeaway" };
+
+Random rnd = new Random();
+
+int randomIndex = rnd.Next(0, snacks.Length);
+
+Console.WriteLine($"Tonight we are going to eat {snacks[randomIndex]}.");
---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,15 @@
+﻿string hello = "We are the champions?";
+
+int stringLength = hello.Length;
+
+hello = hello.Trim();
+
+char firstLetter = hello[0]; 
+Console.Write(firstLetter);
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0])).Replace('?', '!');
+
+firstLetter = hello[0];
+
+Console.WriteLine(firstLetter);
+Console.WriteLine(hello);
